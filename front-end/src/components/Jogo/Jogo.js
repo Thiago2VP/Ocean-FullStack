1b42aec5fcd6b559fd4605eec4b1b11e641fe373
Thiago Pereira <72038599+Thiago2VP@users.noreply.github.com>
2022-08-23 12:49:00 -0300

    botoes, movimento e teste de colisao

--- front-end/src/components/Jogo/Jogo.js
+++ front-end/src/components/Jogo/Jogo.js
@@ -1,19 +1,60 @@
 import './Jogo.css';
-import clouds from "../../assets/clouds.png";
+import nuvens from "../../assets/clouds.png";
 import marioLive from "../../assets/mario.gif";
 import marioDead from "../../assets/game-over.png";
 import pipe from "../../assets/pipe.png";
+import { useRef, useState } from "react";
 
 function Jogo() {
+
+  const [estaPulando, setEstaPulando] = useState(false);
+
+  const marioRef = useRef();
+  const canoRef = useRef();
+
+  function marioEstaNoCano() {
+    //Acessamos as referencias do mario e do cano
+    const mario = marioRef.current;
+    const cano = canoRef.current;
+
+    //Ver se mario e cano existem
+    if (!mario || !cano) {
+      return;
+    }
+
+    //Retorna o valor logico da avaliacao
+    return cano.offsetLeft > mario.offsetLeft && cano.offsetLeft < mario.offsetLeft + mario.offsetWidth && mario.offsetTop + mario.offsetHeight > cano.offsetTop;
+
+  }
+
+  //exibicao etmporaria se mario morreu ou nao
+  setInterval(function () {
+    const valor = marioEstaNoCano();
+
+    console.log("Mario esta no cano?", valor);
+  }, 100);
+
+  document.onkeydown = function () {
+    setEstaPulando(true);
+
+    setTimeout(function () {
+      setEstaPulando(false);
+    }, 1000);
+  }
+
+  let marioClassName = "mario";
+
+  if (estaPulando) {
+    marioClassName = "mario mario-pulo";
+  }
+
   return (
     <div className="Jogo">
-        <div className="acima-do-chao">
-            <img className="nuvens" alt="Nuvens" src={clouds} />
-            <img className="mario" alt="Mario Bros Correndo" src={marioLive} />
-            <img className="morte" alt="mario Bros Derrotado" src={marioDead} />
-            <img className="cano" alt="Cano verde do Mario" src={pipe} />
-        </div>
-        <div className="chao"></div>
+        <img className="nuvens" alt="Nuvens" src={nuvens} />
+        <img ref={marioRef} className={marioClassName} alt="Mario Bros Correndo" src={marioLive} />
+        <img className="morte" alt="mario Bros Derrotado" src={marioDead} />
+        <img ref={canoRef} className="cano" alt="Cano verde do Mario" src={pipe} />
+        <div className="chao" title="Chão"></div>
     </div>
   );
 }
